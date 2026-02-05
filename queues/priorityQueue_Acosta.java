52623294a09cedc149332b30c9d7245d417c24f4
ttop-stack <ttopacosta7@gmail.com>
2026-02-05 20:50:03 +0000

    adding comparator?

--- queues/priorityQueue_Acosta.java
+++ queues/priorityQueue_Acosta.java
@@ -6,7 +6,8 @@ public class priorityQueue_Acosta {
 
         System.out.println("lets make a priority queue!");
 
-        PriorityQueue<Integer> nums = new PriorityQueue<>();
+        // Max heap comparator - largest elements removed first
+        PriorityQueue<Integer> nums = new PriorityQueue<>((a, b) -> b - a);
 
         nums.add(8);
         nums.add(6);
@@ -16,9 +17,15 @@ public class priorityQueue_Acosta {
         nums.add(0);
         nums.add(9);
 
-        System.out.println("Are priority queque is as follows ==> " + nums);
+        System.out.println("Our priority queue is as follows ==> " + nums);
 
-        System.out.println("we will now create a  comprartor that removes the largest numbers first");
+        System.out.println("We created a comparator (a, b) -> b - a  that we placed in the constructor of the priority queue "
+       + "This means that the largest elements will be removed first when we call poll()");
+        
+        System.out.println("lets see how it works");
+        while (!nums.isEmpty()) {
+            System.out.println("Removed ==>" + nums.poll());
+        }
         
         
 
